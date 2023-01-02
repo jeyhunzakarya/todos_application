fa568da8638172f54ad468a5f74cffe87e45768d
JeyhunZakaryayev <zakaryayev@kaist.ac.kr>
2023-01-03 08:20:32 +0900

    changed to root.render

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
 import store from "./redux/store";
 import { Provider } from "react-redux";
-ReactDOM.render(
+import { createRoot } from 'react-dom/client';
+
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<App />
 		</Provider>
-	</React.StrictMode>,
-	document.getElementById('root')
+	</React.StrictMode>
 );
 
